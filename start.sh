07d8604698063439b9a62f2ebb25d859c3bf16b6
markmumba <markmumba01@gmail.com>
2019-08-09 18:20:01 +0300

    all

---
+++ start.sh
@@ -0,0 +1,6 @@
+export MOVIE_API_KEY='51e862f24d51b8aa6c09aa02fde190ff'
+export SECRET_KEY='bobbyshmurda'
+export MAIL_USERNAME='markmumba01@gmail.com'
+export MAIL_PASSWORD='bobbyshmurda'
+
+python3.6 manage.py server
